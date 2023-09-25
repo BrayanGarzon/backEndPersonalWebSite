8cf62fc40310ecbee96eba4f022f73c66afa522c
Brayan Garzon <destroyergarzon@gmail.com>
2023-09-25 18:39:25 -0500

    configuration deploy

--- personalWebSiteBackEnd/settings.py
+++ personalWebSiteBackEnd/settings.py
@@ -1,6 +1,7 @@
 
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -10,12 +11,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kt5^7247ed8zprkh1hb*rmlp)*q&3*^z#86e*(iv%ke#@ml+gw'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False").lower() == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ORIGIN_WHITELIST = (
@@ -127,6 +128,9 @@ DATABASES = {
 }
 
 
+database_url = os.environ.get("DATABASE_URL")
+DATABASES['default'] = dj_database_url.parse(database_url)
+#postgres://mydatabase_q1eg_user:wr1UEvVO90u0H9xGRI49C8NbtV27Zr9F@dpg-ck6tijvsasqs73dlegl0-a.oregon-postgres.render.com/mydatabase_q1eg
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
