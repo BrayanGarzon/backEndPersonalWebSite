dac499c859b92397e4aacf9c83cb9e96e4865eb5
clavijo99 <bclavijogomez@gmail.com>
2023-09-25 23:05:38 -0500

    create super user

--- build.sh
+++ build.sh
@@ -7,13 +7,7 @@ pip install -r ./requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-python manage.py shell <<EOF
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
-username = 'nombre_de_usuario'
-if not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(username, 'correo@example.com', 'contraseña')
-EOF
+python manage.py loaddata initial
+python manage.py loaddata initial dev
 
 
