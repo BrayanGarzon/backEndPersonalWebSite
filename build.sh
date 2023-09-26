74ba215c89c811eede71cf5eb6715a08127e8f9e
clavijo99 <60893557+clavijo99@users.noreply.github.com>
2023-09-25 23:42:29 -0500

    Deploy (#1)
    * deploy v1
    
    * remove flag RUN build.sh
    
    * remove incorrect pat requirements
    
    * install whitenoise
    
    * add whitenoise
    
    * remove create superuser
    
    * settings
    
    * env
    
    * split allowed host
    
    * create super user
    
    * create super user
    
    * create super user
    
    * create super user
    
    * create super user
    
    * create super user
    
    * create super user
    
    * create super user
    
    * create super user
    
    * create super user

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+pip install --upgrade pip
+pip install -r ./requirements.txt
+
+
+python manage.py collectstatic --no-input
+python manage.py migrate
+
+python manage.py loaddata  dev.json
+
+
--- personalWebSiteBackEnd/settings.py
+++ personalWebSiteBackEnd/settings.py
@@ -2,6 +2,9 @@
 import os
 from pathlib import Path
 import dj_database_url
+from dotenv import load_dotenv
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -14,14 +17,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "False").lower() == "True"
+DEBUG = os.environ.get("DEBUG", 'False')
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = os.environ.get(
+    'ALLOWED_HOSTS', ["http://0.0.0.0:8000/", '*']).split(' ')
 
 CORS_ORIGIN_ALLOW_ALL = True
-CORS_ORIGIN_WHITELIST = (
-     'http://localhost:8080',
-)
+
 
 # Application definition
 
@@ -34,13 +36,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'myApp',
-    'rest_framework', #rest
+    'rest_framework',  # rest
     'corsheaders',
-    
+
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -51,23 +54,23 @@ MIDDLEWARE = [
 ]
 
 JAZZMIN_SETTINGS = {
-     # title of the window (Will default to current_admin_site.site_title if absent or None)
+    # title of the window (Will default to current_admin_site.site_title if absent or None)
     "site_title": "DevDynasty",
-     # Title on the brand (19 chars max) (defaults to current_admin_site.site_header if absent or None)
+    # Title on the brand (19 chars max) (defaults to current_admin_site.site_header if absent or None)
     "site_brand": "Panel Admin",
-      # Welcome text on the login screen
+    # Welcome text on the login screen
     "welcome_sign": "Welcome Super Admin",
     # Copyright on the footer
     "copyright": "DevDynasty",
-    
+
     "icons": {
         "auth": "fas fa-users-cog",
         "auth.user": "fas fa-user",
         "auth.Group": "fas fa-users",
-        
+
         "myApp.Categoria": "fas fa-folder",
         "myApp.LenguajesFrameworks": "fas fa-code",
-        
+
         "myApp.Publicacion": "fas fa-upload",
         "myApp.Proyectos": "fas fa-project-diagram"
 
@@ -86,9 +89,9 @@ JAZZMIN_SETTINGS = {
 
 
     # Field name on user model that contains avatar ImageField/URLField/Charfield or a callable that receives the user
-    
+
     "user_avatar": "avatar_url",
-    
+
 
 }
 
@@ -121,9 +124,10 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'myAppBackEnd',   # Nombre de la base de datos que creaste
         'USER': 'postgres',       # Usuario de PostgreSQL
-        'PASSWORD': '1120507188Bg', # Contraseña del usuario de PostgreSQL
-        'HOST': 'localhost',        # Host de la base de datos (puede ser localhost o una IP)
-        'PORT': '5433',       
+        'PASSWORD': '1120507188Bg',  # Contraseña del usuario de PostgreSQL
+        # Host de la base de datos (puede ser localhost o una IP)
+        'HOST': 'localhost',
+        'PORT': '5433',
     }
 }
 
@@ -131,7 +135,7 @@ DATABASES = {
 database_url = os.environ.get("DATABASE_URL")
 DATABASES['default'] = dj_database_url.parse(database_url)
 
-#postgres://mydatabase_q1eg_user:wr1UEvVO90u0H9xGRI49C8NbtV27Zr9F@dpg-ck6tijvsasqs73dlegl0-a.oregon-postgres.render.com/mydatabase_q1eg
+# postgres://mydatabase_q1eg_user:wr1UEvVO90u0H9xGRI49C8NbtV27Zr9F@dpg-ck6tijvsasqs73dlegl0-a.oregon-postgres.render.com/mydatabase_q1eg
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
@@ -166,7 +170,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # Default primary key field type
@@ -175,8 +180,14 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-
 # settings.py
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
+    # in your application directory on Render.
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # Turn on WhiteNoise storage backend that takes care of compressing static files
+    # and creating unique names for each version so they can safely be cached forever.
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
