c3f1bc3ae13f80e1328f1876a1fa662736fa0273
Brayan Garzon <destroyergarzon@gmail.com>
2023-09-26 10:25:25 -0500

    deploy coment line user

--- build.sh
+++ build.sh
@@ -7,7 +7,8 @@ pip install -r ./requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-#echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@gmail.com', 'admin')" | python manage.py shell
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@gmail.com', 'admin')" | python manage.py shell
+
 python manage.py loaddata  dev.json
 
 
